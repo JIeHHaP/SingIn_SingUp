205a1677518c628a0514bc17a8c973c9a5d8668e
Alexey Subbotin <jiehhap@gmail.com>
2021-05-21 00:33:08 +0700

    interface added

---
+++ app/src/main/java/com/example/pseudo_singin_singup/Reg_activity.kt
@@ -0,0 +1,11 @@
+package com.example.pseudo_singin_singup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Reg_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+    }
+}
